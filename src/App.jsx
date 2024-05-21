c7b1cf6b0732774e2c9c024a077ad2189c96d611
sofilevin <sofilevinwilkosz@gmail.com>
2024-05-20 23:16:49 -0300

    todo listo para el parcial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './Components/Card'
+import Form from './Components/Form'
+
+function App() {
+  return (
+    <div className="App">
+    <Card />
+    <Form/>
+  </div>
+  );
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+
+
+const Card = () => {
+  return (
+    <div>Hola soy una card</div>
+  )
+}
+
+export default Card
